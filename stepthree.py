40866f225a81c40eb58d6dd7208bae5fc74d9d9b
Mia Gil Epner <miagilepner@berkeley.edu>
2016-10-08 10:00:02 -0700

    added step 3

---
+++ stepthree.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+payload = {"token":"cb4d2e35a6d1ba61efbae8d6273d7e43"}
+r = requests.post("http://challenge.code2040.org/api/haystack", json=payload)
+jsonresp = r.json()
+needle = jsonresp["needle"]
+haystack = jsonresp["haystack"]
+index = haystack.get(needle)
+payload.update({"needle":index})
+re = requests.post("http://challenge.code2040.org/api/haystack/validate", json=payload)
+print re.text
