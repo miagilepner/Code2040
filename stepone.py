5a50a9bb7d2b876e14e7cfa136329fbd96e8b3e3
Mia Gil Epner <miagilepner@berkeley.edu>
2016-10-08 09:35:20 -0700

    imported json

--- stepone.py
+++ stepone.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 payload = {"token":"cb4d2e35a6d1ba61efbae8d6273d7e43", "github":"https://github.com/miagilepner/Code2040"}
 r = requests.post("http://challenge.code2040.org/api/register", json=payload)
