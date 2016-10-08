1eb6d5b315018068fa8e285f942e13fc009d35ec
Mia Gil Epner <miagilepner@berkeley.edu>
2016-10-08 09:51:19 -0700

    step two

--- steptwo.py
+++ steptwo.py
@@ -3,7 +3,7 @@ import json
 
 payload = {"token":"cb4d2e35a6d1ba61efbae8d6273d7e43"}
 r = requests.post("http://challenge.code2040.org/api/reverse", json=payload)
-toreverse = r.json().get("string")
+toreverse = r.text
 payload.update({"string":toreverse[::-1]})
 re = requests.post("http://challenge.code2040.org/api/reverse", json=payload)
 print re.text
